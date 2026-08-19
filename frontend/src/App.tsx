eb04e5b765c4163fc9ce80e1149ebc5db5533150
Jorge Koch <jorgeluizkoch@gmail.com>
2026-08-19 19:09:40 -0300

    feat: add triage and admin routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,20 +7,19 @@ import { FaqSection } from './components/sections/FaqSection'
 import { HeroSection } from './components/sections/HeroSection'
 import { ProcessSection } from './components/sections/ProcessSection'
 import { SpecialtiesSection } from './components/sections/SpecialtiesSection'
+import { TriagePage } from './components/pages/TriagePage'
+import { AdminLoginPage } from './components/pages/AdminLoginPage'
+import { AdminTriagePage } from './components/pages/AdminTriagePage'
+import { AdminTriageDetailPage } from './components/pages/AdminTriageDetailPage'
 
 function App() {
-  return (
-    <SiteLayout>
-      <HeroSection />
-      <AboutSection />
-      <ProcessSection />
-      <SpecialtiesSection />
-      <CredentialsSection />
-      <ClinicSection />
-      <FaqSection />
-      <CtaSection />
-    </SiteLayout>
-  )
+  const path = window.location.pathname.replace(/\/$/, '') || '/'
+  if (path === '/triagem') return <TriagePage />
+  if (path === '/admin/login') return <AdminLoginPage />
+  if (path === '/admin/triagens') return <AdminTriagePage />
+  if (path.startsWith('/admin/triagens/')) return <AdminTriageDetailPage id={path.split('/').pop() ?? ''} />
+
+  return <SiteLayout><HeroSection /><AboutSection /><ProcessSection /><SpecialtiesSection /><CredentialsSection /><ClinicSection /><FaqSection /><CtaSection /></SiteLayout>
 }
 
 export default App
